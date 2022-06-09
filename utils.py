5941a094a691019e050d4ac9c623ff59d78e3664
FangZhiGao <623290520@qq.com>
2022-06-09 18:40:46 +0800

    增加进度条类

--- utils.py
+++ utils.py
@@ -25,6 +25,23 @@ class AverageMeter(object):
         self.count = 0
 
 
+class ProgressMeter(object):
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    def _get_batch_fmtstr(self, num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
 def get_logger(logpath, resume=False):
     formatter = logging.Formatter('%(asctime)s %(levelname)s - %(funcName)s: %(message)s', "%H:%M:%S")
     logger = logging.getLogger(__name__)
